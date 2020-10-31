0078604d4394091d111f4e43588dd6d0ff95a334
Michel Cadennes <michel.cadennes@assemblee-virtuelle.org>
2020-10-31 18:49:51 +0100

    Ajout de données statique dans le contrôleur
    Une liste d'auteurs et de leurs œuvres.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,12 +9,48 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
+     * Source de données statique
+     * Une liste d'auteurs et de leurs œuvres
+     */
+    const AUTHORS = [
+        [ 'name' => 'Victor Hugo', 'works' => [
+            ['title' => 'Notre-Dame de Paris', "category" => 'roman'],
+            ['title' => 'Les Misérables', "category" => 'roman'],
+            ['title' => 'Les Contemplations', "category" => 'poésie']
+        ]
+        ],
+        [ 'name' => 'Thomas Mann', 'works' => [
+            ['title' => 'Doktor Faustus', "category" => 'roman'],
+            ['title' => 'Les Buddenbrock', "category" => 'roman']
+        ]
+        ],
+        [ 'name' => 'William Shakespeare', 'works' => [
+            ['title' => 'Le roi Lear', "category" => 'théâtre'],
+            ['title' => 'Les Sonnets', "category" => 'poésie']
+        ]
+        ],
+        [ 'name' => 'François Rabelais', 'works' => [
+            ['title' => 'Le Tiers-Livre', "category" => 'roman'],
+            ['title' => 'Le Quart-Livre', "category" => 'roman']
+        ]
+        ],
+        [ 'name' => 'Platon', 'works' => [
+            ['title' => 'La République', "category" => 'essai'],
+            ['title' => 'Le Timée', "category" => 'essai']
+        ]
+        ],
+    ];
+    /**
      * @Route("/default", name="default")
      */
     public function index(): Response
     {
         return $this->render('default/index.html.twig', [
-            'authors' => [],
+            /*
+             *  Le tableau est envoyé à Twig pour affichage.
+             *  Dans Twig, la variable porter le nom de la clef dans le tableau (authors)
+             */
+            'authors' => self::AUTHORS,
         ]);
     }
 }
